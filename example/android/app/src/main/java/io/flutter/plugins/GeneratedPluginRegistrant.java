8f45449752f6dcf1061a713f65a43d75b94dc3c1
SwirX <swirxytb@gmail.com>
2024-01-03 23:41:32 +0100

    updated the sdk version

--- example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,6 +2,7 @@ package io.flutter.plugins;
 
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
+import io.flutter.Log;
 
 import io.flutter.embedding.engine.FlutterEngine;
 
@@ -12,7 +13,12 @@ import io.flutter.embedding.engine.FlutterEngine;
  */
 @Keep
 public final class GeneratedPluginRegistrant {
+  private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
-    flutterEngine.getPlugins().add(new com.pinciat.storage_info.StorageInfoPlugin());
+    try {
+      flutterEngine.getPlugins().add(new com.pinciat.storage_info.StorageInfoPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin storage_info, com.pinciat.storage_info.StorageInfoPlugin", e);
+    }
   }
 }
